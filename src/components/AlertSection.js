fb711f170d3c33a34fe8721b972c6be8bcabf23e
Stackbit Code Editor <projects@stackbit.com>
2021-11-16 05:58:01 +0000

    AlertSection.js: changed

--- src/components/AlertSection.js
+++ src/components/AlertSection.js
@@ -1,8 +1,12 @@
 const AlertSection = ({ annotationPrefix, body }) => {
     return (
-        <div data-sb-field-path={annotationPrefix}>
-            <p data-sb-field-path=".body">{body}</p>
-        </div>
+        <div data-sb-field-path={annotationPrefix} className="py-6 bg-primary">
+      <div className="container mx-auto">
+        <p data-sb-field-path=".body" className="text-center text-xl text-white">
+          {body}
+        </p>
+      </div>
+    </div>
     )
     
 }
